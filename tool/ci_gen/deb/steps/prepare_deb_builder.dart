f6a4119def627b8559a06560d2934adcf5b8d776
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-04-07 12:13:09 +0200

    fix makedeb install

--- tool/ci_gen/deb/steps/prepare_deb_builder.dart
+++ tool/ci_gen/deb/steps/prepare_deb_builder.dart
@@ -1,6 +1,5 @@
 import '../../common/api/step_builder.dart';
 import '../../common/globals.dart';
-import '../../types/env.dart';
 import '../../types/step.dart';
 
 class PrepareDebBuilder implements StepBuilder {
@@ -10,27 +9,30 @@ class PrepareDebBuilder implements StepBuilder {
           name: 'Add dart repository',
           run: '''
 set -eo pipefail
-wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/dart.gpg
+wget -qO- 'https://dl-ssl.google.com/linux/linux_signing_key.pub' | sudo gpg --dearmor -o /usr/share/keyrings/dart.gpg
 echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list
-sudo apt-get update
 ''',
         ),
         Step.run(
-          name: 'Install dart',
-          run: 'sudo apt-get install -y dart',
+          name: 'Add makedeb repository',
+          run: '''
+set -eo pipefail
+wget -qO- 'https://proget.makedeb.org/debian-feeds/makedeb.pub' | sudo gpg --dearmor -o /usr/share/keyrings/makedeb-archive-keyring.gpg
+echo 'deb [signed-by=/usr/share/keyrings/makedeb-archive-keyring.gpg arch=all] https://proget.makedeb.org/ makedeb main' | sudo tee /etc/apt/sources.list.d/makedeb.list
+''',
+        ),
+        Step.run(
+          name: 'Refresh packages cache',
+          run: 'sudo apt-get update',
+        ),
+        Step.run(
+          name: 'Install needed packages',
+          run: 'sudo apt-get install -y dart makedeb',
         ),
         const Step.run(
           name: 'Install dart_test_tools',
           run:
               'dart pub global activate dart_test_tools ^$dartTestToolsVersion',
-        ),
-        Step.run(
-          name: 'Install makedeb',
-          env: const Env({
-            'MAKEDEB_RELEASE': 'makedeb',
-          }),
-          run:
-              'bash -c "\$(wget -qO - \'https://shlink.makedeb.org/install\')"',
         ),
         Step.run(
           name: 'Disable stripping',
