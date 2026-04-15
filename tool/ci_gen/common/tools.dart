1d49591888e85b77ba039e7d85a45d9cd2b38f96
Skycoder42 <Skycoder42@users.noreply.github.com>
2026-04-15 07:07:11 +0200

    update tools

--- tool/ci_gen/common/tools.dart
+++ tool/ci_gen/common/tools.dart
@@ -35,7 +35,7 @@ abstract class Tools {
   static const subositoFlutterAction = 'subosito/flutter-action@v2';
 
   /// https://github.com/softprops/action-gh-release/releases
-  static const softpropsActionGhRelease = 'softprops/action-gh-release@v2';
+  static const softpropsActionGhRelease = 'softprops/action-gh-release@v3';
 
   /// https://github.com/VeryGoodOpenSource/very_good_coverage/releases
   static const veryGoodOpenSourceVeryGoodCoverage =
